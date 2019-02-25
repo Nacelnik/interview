bbe4c62bf03f19f8edcec0f19a04dae54bba5e02
petr.vymola <p.vymola@gmail.com>
2019-02-25 21:50:44 +0100

    clean run

--- first-run.sh
+++ first-run.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 mvn clean package dockerfile:build
-
-docker network create closeit-interview-pv-net
-docker run -d -p 27017:27017 --name closeit-interview-db mongo --network=closeit-interview-pv-net
-
-docker run -p 5000:8080 --name closeit-interview-app closeit-interview:0.0.1 --network=closeit-interview-pv-net
+docker-compose up
--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-docker start closeit-interview-db
-docker start closeit-interview-app
